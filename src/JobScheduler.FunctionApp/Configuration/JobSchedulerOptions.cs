71f686f7750d62aa6859e8f7bcb47e3a79f51f67
Ryan Waszak <rwaszak@gocontinuum.ai>
2025-08-09 09:43:45 -0600

    unnecessary attribute

--- src/JobScheduler.FunctionApp/Configuration/JobSchedulerOptions.cs
+++ src/JobScheduler.FunctionApp/Configuration/JobSchedulerOptions.cs
@@ -1,12 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace JobScheduler.FunctionApp.Configuration;
 
 public class JobSchedulerOptions
 {
     public const string SectionName = "JobScheduler";
 
-    [Required]
     public Dictionary<string, JobDefinition> Jobs { get; init; } = new();
     
     public LoggingOptions Logging { get; init; } = new();
